0b4c91fae64f720c5039c56e223d4914cb9ed986
Brittiany Juravic <britjuravic@Brittianys-MacBook-Pro.local>
2020-03-31 12:31:21 -0700

    Parts 1 and 2 compelte

---
+++ models.py
@@ -0,0 +1,29 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+
+
+def connect_db(app):
+    """Connect to database."""
+    db.app = app
+    db.init_app(app)
+
+
+class User(db.Model):
+  """User class."""
+
+  __tablename__ = "users"
+
+  username = db.Column(db.String(20),
+                       primary_key=True)
+  password = db.Column(db.Text,
+                       nullable=False)
+  email = db.Column(db.String(50),
+                    nullable=False,
+                    unique=True)
+  first_name = db.Column(db.String(30),
+                         nullable=False)
+  last_name = db.Column(db.String(30),
+                        nullable=False)
